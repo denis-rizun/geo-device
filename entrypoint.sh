1a395b54e3cac93c197ca85a9e65fd6d68262b97
denis <d.rizun.work@gmail.com>
2026-09-15 07:59:42 +0200

    chore(docker): containerize project

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+case "$1" in
+  api)
+    exec uvicorn app.main:app --host 0.0.0.0 --port "${API_PORT:-8000}" --workers "${API_WORKERS:-1}"
+    ;;
+  migrate)
+    exec alembic upgrade head
+    ;;
+  generate)
+    shift
+    exec python -m app.generator "$@"
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
